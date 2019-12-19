e3ac4105555bc63eda17162ec03686996086d94b
CJ Wong <cj-wong@users.noreply.github.com>
2019-12-18 22:32:47 -0800

    Update astronomy to include asteroids; adapt astronomy to solutions

--- 2019/10/b.py
+++ 2019/10/b.py
@@ -3,6 +3,8 @@ import sys
 from collections import defaultdict
 from typing import List, Tuple
 
+import astronomy
+
 
 INPUTS = [
     [ # Coordinate: (3, 4)
@@ -15,165 +17,7 @@ INPUTS = [
     ]
 
 
-ASTEROIDS = defaultdict(str)
-VAPORIZED = []
-
 X, Y = (28, 29)
-
-
-LINE_OF_SIGHT = []
-
-
-def map_asteroids(ast_map: List[str]) -> None:
-    """Map asteroids given an `ast_map`.
-
-    Args:
-        ast_map (List[str]): the asteroid map
-
-    """
-    for y, row in enumerate(ast_map):
-        for x, char in enumerate(row):
-            if char == '#':
-                ASTEROIDS[(x, y)] = '#'
-
-
-def is_visible(f: Tuple[int], x: int, y: int) -> bool:
-    """Is the asteroid at (x,y) with a vector f visible?
-    Removes existing asteroid(s) blocked by this one if they both share
-    the same reduced vector.
-
-    Args:
-        f (Tuple[int]): the vector of the asteroid
-        x (int): x-coordinate of the asteroid
-        y (int): y-coordinate of the asteroid
-
-    Returns:
-        bool: True if visible, even if a previously existing asteroid
-            has the same vector but is blocked by the one checked in
-            this function; False otherwise
-
-    """
-    dx = abs(x - X)
-    dy = abs(y - Y)
-    for line, (x0, y0) in LINE_OF_SIGHT:
-        if line == f:
-            dx0 = abs(x0 - X)
-            dy0 = abs(y0 - Y)
-            if dx < dx0 or dy < dy0:
-                LINE_OF_SIGHT.remove((line, (x0, y0)))
-                return True
-            else:
-                return False
-    return True
-
-
-def map_sights() -> List[Tuple[int]]:
-    """Map asteroids in sight.
-
-    Returns:
-        List[Tuple[int]]: a list of coordinates of seen asteroids
-
-    """
-    for (x, y) in ASTEROIDS:
-        if x == X and y == Y:
-            continue
-        f = Vector(x - X, y - Y).to_tuple()
-        result = is_visible(f, x, y)
-
-        if result:
-            LINE_OF_SIGHT.append((f, (x, y)))
-
-    return [(x, y) for _, (x, y) in LINE_OF_SIGHT]
-
-
-def count_vaporized(
-    n: int, quadrant: List[Tuple[int]], reverse: bool = False
-    ) -> int:
-    """Count vaporized asteroids and if `n` is greater than or equal to
-    200, sort the `quadrant` and output the 200th.
-
-    Args:
-        n (int): the current number of vaporized asteroids
-        quadrant (List[Tuple[int]]): list of coordinates in a quadrant
-        reverse (bool, optional): whether to reverse sort;
-            defaults to False
-
-    Returns:
-        int: `n`, after processing `quadrant`
-
-    """
-    if n >= 200 - len(quadrant):
-        results = [
-            ((x, y), abs(math.atan2(y - Y, x - X)))
-            for (x, y)
-            in quadrant
-            ]
-        fx, fy = sorted(
-            results,
-            key=lambda r: r[1],
-            reverse=reverse
-            )[199-n][0]
-        print(fx * 100 + fy)
-        sys.exit(0)
-    return n + len(quadrant)
-
-
-def vaporize(seen: List[Tuple[int]]) -> None:
-    """Vaporize seen asteroids.
-
-    Args:
-        seen (List[Tuple[int]]): a list of seen asteroids' coordinates
-
-    """
-    records = []
-    n = 0
-    # These aren't the same as mathematical quadrants. Because the
-    # "sweeping" arm rotates clockwise, the mathematical q4 is the
-    # second quadrant to be reached. Likewise for below q's.
-    # Furthermore, the y-axis is inverse-order; the "top" is negative
-    # relative to the center.
-    q1 = [(x, y) for (x, y) in seen if x >= X and y < Y]
-    n = count_vaporized(n, q1, reverse=True)
-    # Not the mathematical q2. 
-    q2 = [(x, y) for (x, y) in seen if x >= X and y >= Y]
-    n = count_vaporized(n, q2)
-    q3 = [(x, y) for (x, y) in seen if x < X and y >= Y]
-    n = count_vaporized(n, q3)
-    q4 = [(x, y) for (x, y) in seen if x < X and y < Y]
-    n = count_vaporized(n, q4, reverse=True)
-
-
-class Vector:
-    """A 2D vector."""
-    def __init__(self, x: int, y: int) -> None:
-        """Initialize the vector using both axes. Tries to reduce
-        the vector if possible.
-
-        Args:
-            x (int): the x-axis offset
-            y (int): the y-axis offset
-
-        """
-        if x == 0:
-            self.x = x
-            if y > 0:
-                self.y = 1
-            else:
-                self.y = -1
-        elif y == 0:
-            self.y = y
-            if x > 0:
-                self.x = 1
-            else:
-                self.x = -1
-        else:
-            gcd = math.gcd(x, y)
-            self.x = x // gcd
-            self.y = y // gcd
-
-    def to_tuple(self) -> Tuple[int, int]:
-        """Create a tuple representation of the vector."""
-        return (self.x, self.y)
 
 
 def main() -> None:
@@ -183,11 +27,12 @@ def main() -> None:
     #     map_sights()
 
     with open('input', 'r') as f:
-        a_map = f.read().strip().split('\n')
+        a_map = f.read().strip()
 
-    map_asteroids(a_map)
-    seen = map_sights()
-    vaporize(seen)
+    a_map = astronomy.AsteroidField(a_map)
+    center = a_map.asteroids[X, Y]
+    center.map_seen(a_map)
+    center.vaporize_until(a_map, 200)
 
 
 if __name__ == '__main__':
--- 2019/common/astronomy.py
+++ 2019/common/astronomy.py
@@ -1,6 +1,260 @@
-from typing import List
+import math
+import sys
+from collections import defaultdict
+from typing import List, Tuple
 
 
+# Day 6
+class Body:
+    """Represents a celestial body with orbits.
+
+    Attributes:
+        name (str): name of the celestial body
+        parent (Body): this celestial body's parent
+
+    """
+    def __init__(self, name: str) -> None:
+        """Initialize using a name.
+
+        Args:
+            name (str): name of this celestial body
+
+        """
+        self.name = name
+        self.parent = None
+
+    def add_parent(self, body: 'Body') -> None:
+        """Add a direct parent `body`.
+
+        Args:
+            body (Body): the parent body
+
+        """
+        self.parent = body
+
+
+# Day 10
+class Asteroid:
+    """Represents an asteroid.
+
+    Attributes:
+        x (int): the x-coordinate of the asteroid
+        y (int): the y-coordinate of the asteroid
+        seen (List[Tuple[int]]): a list of directly seen asteroids by
+            their coordinates (x,y)
+
+    """
+    def __init__(self, x: int, y: int) -> None:
+        """Initialize the asteroid with its coordinates.
+
+        Args:
+            x (int): the x-coordinate of the asteroid
+            y (int): the y-coordinate of the asteroid
+
+        """
+        self.x = x
+        self.y = y
+        self.seen = []
+
+    def is_visible_from(self, v: Tuple[int], x: int, y: int) -> bool:
+        """Is the asteroid at (x,y) with a vector f visible?
+        Removes existing asteroid(s) blocked by this one if they both share
+        the same reduced vector.
+
+        Args:
+            v (Tuple[int]): the reduced vector of the asteroid
+            x (int): x-coordinate of the asteroid potentially seen from
+                this asteroid
+            y (int): y-coordinate of the asteroid potentially seen from
+                this asteroid
+
+        Returns:
+            bool: True if visible, even if a previously existing asteroid
+                has the same vector but is blocked by the one checked in
+                this function; False otherwise
+
+        """
+        dx = abs(x - self.x)
+        dy = abs(y - self.y)
+        for v0, (x0, y0) in self.seen:
+            if v0 == v:
+                dx0 = abs(x0 - self.x)
+                dy0 = abs(y0 - self.y)
+                if dx < dx0 or dy < dy0:
+                    self.seen.remove((v0, (x0, y0)))
+                    return True
+                else:
+                    return False
+        return True
+
+    def map_seen(self, a_map: 'AsteroidField') -> None:
+        """Map asteroids that this asteroid can see.
+
+        Args:
+            a_map (AsteroidField): the map of the asteroid field
+
+        """
+        for (x, y), asteroid in a_map.asteroids.items():
+            if asteroid == self:
+                continue
+            v = Vector(x - self.x, y - self.y).to_tuple()
+            result = self.is_visible_from(v, x, y)
+
+            if result:
+                self.seen.append((v, (x, y)))
+
+    def count_vaporized(
+        self, n: int, target: int, quadrant: List[Tuple[int]],
+        reverse: bool = False
+        ) -> int:
+        """Count vaporized asteroids and if `n` is greater than or equal to
+        200, sort the `quadrant` and output the 200th.
+
+        Args:
+            n (int): the current number of vaporized asteroids
+            quadrant (List[Tuple[int]]): list of coordinates in a quadrant
+            reverse (bool, optional): whether to reverse sort;
+                defaults to False
+
+        Returns:
+            int: `n`, after processing `quadrant`
+
+        """
+        if n >= target - len(quadrant):
+            results = [
+                ((x, y), abs(math.atan2(dy, dx)))
+                for (dx, dy, x, y)
+                in quadrant
+                ]
+            fx, fy = sorted(
+                results,
+                key=lambda r: r[1],
+                reverse=reverse
+                )[target - 1 - n][0]
+            print(f'Vaporized #{target}: ({fx},{fy}); answer:', fx * 100 + fy)
+            sys.exit(0)
+        return n + len(quadrant)
+
+    def vaporize_until(self, a_map: 'AsteroidField', target: int = 200) -> None:
+        """Vaporize asteroids directly seen from this one.
+
+        Note: if a KeyError is raised, you have set `target` higher
+        than the total of asteroids.
+
+        Args:
+            a_map (AsteroidField): a map of asteroids
+            target (int): the nth desired target of vaporization
+
+        """
+        records = []
+        n = 0
+        while n < target:
+            # These aren't the same as mathematical quadrants. Because
+            # the "sweeping" arm rotates clockwise, the mathematical q4
+            # is the second quadrant to be reached. Likewise for below
+            # q's. Furthermore, the y-axis is inverse-order; the "top"
+            # is negative relative to the center.
+            q1 = [
+                (dx, dy, x, y)
+                for ((dx, dy), (x, y))
+                in self.seen
+                if x >= self.x and y < self.y
+                ]
+            n = self.count_vaporized(n, target, q1, reverse=True)
+            q2 = [
+                (dx, dy, x, y)
+                for ((dx, dy), (x, y))
+                in self.seen
+                if x >= self.x and y >= self.y
+                ]
+            n = self.count_vaporized(n, target, q2)
+            q3 = [
+                (dx, dy, x, y)
+                for ((dx, dy), (x, y))
+                in self.seen
+                if x < self.x and y >= self.y
+                ]
+            n = self.count_vaporized(n, target, q3)
+            q4 = [
+                (dx, dy, x, y)
+                for ((dx, dy), (x, y))
+                in self.seen if x < self.x and y < self.y
+                ]
+            n = self.count_vaporized(n, target, q4, reverse=True)
+
+            # Haven't reached the nth? Delete all vaporized asteroids.
+            for (_, (x, y)) in self.seen:
+                del a_map.asteroids[(x, y)]
+
+            # Get the next set of available seen asteroids.
+            self.map_seen(a_map)
+
+
+
+class AsteroidField:
+    """Represents a cluster/map of asteroids.
+
+    Attributes:
+        asteroids (Dict[Tuple[int], Asteroid]): a pseudo-map of
+            asteroids, with keys as coordinates (x,y)
+
+    """
+    def __init__(self, a_map: str) -> None:
+        """Map asteroids given an `a_map`. The first line is treated as
+        y = 0, and the first character of each line is treated as
+        x = 0.
+
+        Args:
+            a_map (str): the asteroid map, as a multiline string
+
+        """
+        self.asteroids = {}
+        for y, row in enumerate(a_map.split('\n')):
+            for x, char in enumerate(row):
+                if char == '#':
+                    self.asteroids[(x, y)] = Asteroid(x, y)
+
+
+class Vector:
+    """A simple 2D vector. Used for calculating direction of asteroids.
+
+    Attributes:
+        x (int): the x-coordinate of the vector
+        y (int): the y-coordinate of the vector
+
+    """
+    def __init__(self, x: int, y: int) -> None:
+        """Initialize the vector using both axes. Tries to reduce
+        the vector if possible.
+
+        Args:
+            x (int): the x-axis offset
+            y (int): the y-axis offset
+
+        """
+        if x == 0:
+            self.x = x
+            if y > 0:
+                self.y = 1
+            else:
+                self.y = -1
+        elif y == 0:
+            self.y = y
+            if x > 0:
+                self.x = 1
+            else:
+                self.x = -1
+        else:
+            gcd = math.gcd(x, y)
+            self.x = x // gcd
+            self.y = y // gcd
+
+    def to_tuple(self) -> Tuple[int, int]:
+        """Create a tuple representation of the vector."""
+        return (self.x, self.y)
+
+
+# Day 12
 class Moon:
     """Represents a moon (of Jupiter).
 
@@ -54,20 +308,3 @@ class Moon:
         self.potential = sum([abs(co) for co in self.position])
         self.kinetic = sum([abs(co) for co in self.velocity])
         self.total_energy = self.potential * self.kinetic
-
-
-class Body:
-    """Represents a celestial body with orbits."""
-    def __init__(self, name: str) -> None:
-        """Initialize using a name."""
-        self.name = name
-        self.parent = None
-
-    def add_parent(self, body) -> None:
-        """Add a direct parent `body`.
-
-        Args:
-            body (CelestialBody): the parent body
-
-        """
-        self.parent = body
