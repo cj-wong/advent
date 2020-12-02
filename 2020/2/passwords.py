8b788447b18a9afff0be242fac9e80f304d97f65
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-02 00:26:36 -0800

    Clean up documentation

--- 2020/2/passwords.py
+++ 2020/2/passwords.py
@@ -13,8 +13,8 @@ class Password:
 
         Args:
             policy (str): in format x-y z, where:
-                x: left value (min value of z in A., position in B.)
-                y: right value (max value of z in A., position in B.)
+                x: left value (min count of z in A., position in B.)
+                y: right value (max count of z in A., position in B.)
                 z: a character requirement within password
             password (str): the password
 
@@ -23,17 +23,25 @@ class Password:
         counts, self.char = policy.split(' ')
         self.l_value, self.r_value = [int(n) for n in counts.split('-')]
 
-    def validate_a(self) -> None:
-        """Validate using policy from part a. From sled rental policy."""
+    def validate_a(self) -> bool:
+        """Validate using policy from part a. From sled rental policy.
+
+        Returns:
+            bool: True if password is valid; False otherwise
+
+        """
         return (
             self.password.count(self.char)
             in range(self.l_value, self.r_value + 1)
             )
 
-    def validate_b(self) -> None:
+    def validate_b(self) -> bool:
         """Validate using policy from part b.
 
         From Official Toboggan Corporate Authentication System.
+
+        Returns:
+            bool: True if password is valid; False otherwise
 
         """
         return (
