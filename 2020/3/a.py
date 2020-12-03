adc53b9340d8ffe269ba7d664b4855bff7ca12d5
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-02 23:07:55 -0800

    Add solution for part b within part a; adjust function to use slopes

--- 2020/3/a.py
+++ 2020/3/a.py
@@ -3,7 +3,7 @@ from typing import List
 import config
 
 
-def count_trees(tree_map: List[str]) -> int:
+def count_trees(tree_map: List[str], dx: int, dy: int) -> int:
     """Count trees (`#`) in the tree map.
 
     Args:
@@ -13,9 +13,10 @@ def count_trees(tree_map: List[str]) -> int:
         int: number of trees
 
     """
-    dx = 3
     count = 0
-    for i, row in enumerate(tree_map[1:], start=1):
+    for i, row in enumerate(tree_map[::dy]):
+        if i == 0:
+            continue
         index = (i * dx) % len(row)
         if row[index] == '#':
             count += 1
@@ -25,14 +26,33 @@ def count_trees(tree_map: List[str]) -> int:
 
 def main() -> None:
     """Determine path through a map of trees."""
+    # Part A
     test_answer = 7
     file = config.TestFile(test_answer)
-    count = count_trees(file.contents)
+    count = count_trees(file.contents, 3, 1)
     file.test(count)
 
+    # Part B
+    test_answer = 336 # = 2 * 7 * 3 * 4 * 2
+    file = config.TestFile(test_answer)
+    slopes = [
+        (1, 1),
+        # (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+        ]
+    for slope in slopes:
+        count *= count_trees(file.contents, *slope)
+    file.test(count)
+
+    # Part A
     file = config.File()
-    count = count_trees(file.contents)
-    print(count)
+    count = count_trees(file.contents, 3, 1)
+    config.LOGGER.info(f'A: {count}')
+    for slope in slopes:
+        count *= count_trees(file.contents, *slope)
+    config.LOGGER.info(f'B: {count}')
 
 
 if __name__ == '__main__':
