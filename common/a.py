8e5fe4bba5f26584d73cd12cd5a903c3f2100911
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-03 21:29:58 -0800

    Update common barebones file

--- common/a.py
+++ common/a.py
@@ -1,18 +1,29 @@
+from typing import List
+
 import config
+
+
+def TODO(contents: List[str]) -> None:
+    """TODO example function.
+
+    Args:
+        contents (List[str]): the file contents
+
+    """
+    for line in contents:
+        pass
 
 
 def main() -> None:
     """TODO."""
     test_answer = 0
     file = config.TestFile(test_answer)
-    contents = file.contents
-    for line in contents:
-        pass
+    test = TODO(file.contents)
+    file.test(test)
 
     file = config.File()
-    contents = file.contents
-    for line in contents:
-        pass
+    result = TODO(file.contents)
+    config.LOGGER.info(result)
 
 
 if __name__ == '__main__':
