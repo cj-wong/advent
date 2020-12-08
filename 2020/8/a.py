5f5aecef1266d754a83ad489e64c0f49b1a19cf1
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-07 22:49:28 -0800

    Add solution for 2020-12-07 b in a.py

--- 2020/8/a.py
+++ 2020/8/a.py
@@ -1,6 +1,62 @@
+import re
 from typing import List
 
 import config
+
+
+OP_RE = re.compile(r'^(jm|no)p')
+
+
+def replace_bad_op(instructions: List[str]) -> int:
+    """Replace the bad op that causes infinite loop and get the accumulator.
+
+    Args:
+        instructions (List[str]): the game's instructions
+
+    Returns:
+        int: the accumulator value after replacing the bad op
+
+    """
+    # Record all indices where swapping is allowed.
+    # In other words, every index in this list corresponds to either
+    # 'jmp' or 'nop' operators.
+    swap = [i for i, op in enumerate(instructions) if OP_RE.search(op)]
+
+    while True:
+        visited = set()
+        step = 0
+        acc = 0
+        swapped = None
+        while step not in visited:
+            visited.add(step)
+            # Because a valid program is considered when the instruction
+            # pointer ends up out of bounds, check for IndexError for
+            # the step against the whole instructions list.
+            try:
+                op, value = instructions[step].split(' ')
+                if swapped == step or (swapped is None and step in swap):
+                    op = 'jmp' if op == 'nop' else 'nop'
+                    swapped = step
+            except IndexError:
+                return acc
+
+            if op == 'nop':
+                step += 1
+            elif op == 'acc':
+                step += 1
+                acc += int(value)
+            elif op == 'jmp':
+                step += int(value)
+            else:
+                config.LOGGER.error(f'Unidentified op code: {op}')
+
+        # if step > len(instructions):
+        #     return acc
+
+        # The loop exited without going out of bounds (and returning),
+        # meaning that this was the wrong operation to swap. Remove it
+        # so that next loop it won't be changed.
+        swap.remove(swapped)
 
 
 def iterate_instructions_once(instructions: List[str]) -> int:
@@ -34,6 +90,7 @@ def iterate_instructions_once(instructions: List[str]) -> int:
 
 def main() -> None:
     """Process game instructions."""
+    # Part A
     test_answer = 5
     file = config.TestFile(test_answer)
     test = iterate_instructions_once(file.contents)
@@ -41,7 +98,17 @@ def main() -> None:
 
     file = config.File()
     result = iterate_instructions_once(file.contents)
-    config.LOGGER.info(result)
+    config.LOGGER.info(f'A: {result}')
+
+    # Part B
+    test_answer = 8
+    file = config.TestFile(test_answer)
+    test = replace_bad_op(file.contents)
+    file.test(test)
+
+    file = config.File()
+    result = replace_bad_op(file.contents)
+    config.LOGGER.info(f'B: {result}')
 
 
 if __name__ == '__main__':
