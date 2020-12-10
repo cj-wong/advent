662af8214aff8d646f8266bad011db6bb1c19e0a
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-09 23:05:43 -0800

    Add solution for 2020-12-10 a

---
+++ 2020/10/a.py
@@ -0,0 +1,48 @@
+from typing import List
+
+import config
+
+
+def calculate_diff_occurrence(joltages: List[str]) -> None:
+    """Calculate diff occurrences for differences of 1 and 3.
+
+    Args:
+        joltages (List[str]): the joltages of the adapters on hand
+
+    """
+    device_joltage = max(joltages) + 3
+    lowest = 1
+    highest = 3
+    # Add both the starting (outlet) and ending (device) joltages.
+    joltages.append(0)
+    joltages.append(device_joltage)
+    joltages = sorted(joltages)
+    diffs = [
+        joltages[i + 1] - joltage for i, joltage in enumerate(joltages[:-1])
+        ]
+    diffs = {diff: diffs.count(diff) for diff in range(lowest, highest + 1)}
+    return diffs[lowest] * diffs[highest]
+
+
+def main() -> None:
+    """Check joltages of the adapters from the outlet to device."""
+    test_answer = 35
+    file = config.TestFile(test_answer)
+    file.contents_to_type(int)
+    test = calculate_diff_occurrence(file.contents)
+    file.test(test)
+
+    test_answer = 220
+    file = config.TestFile(test_answer, path="another_test_input.txt")
+    file.contents_to_type(int)
+    test = calculate_diff_occurrence(file.contents)
+    file.test(test)
+
+    file = config.File()
+    file.contents_to_type(int)
+    result = calculate_diff_occurrence(file.contents)
+    config.LOGGER.info(result)
+
+
+if __name__ == '__main__':
+    main()
