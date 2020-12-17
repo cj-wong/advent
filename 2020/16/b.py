932cc91db6a1783cd46ef484e6493634a0e46ff8
CJ Wong <cj-wong@users.noreply.github.com>
2020-12-16 21:40:12 -0800

    Add solution for 2020-12-16 b.
    - Change "rule" to "field". The "rules" at the top of the notes are for fields.
    - Change regex to include space. This probably stopped several fields from being read.
    - Rename functions to more accurately suit their purposes.
    - Utilize set intersection to determine all columns' matching fields.

--- 2020/16/b.py
+++ 2020/16/b.py
@@ -1,16 +1,17 @@
 import re
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import config
 
 
-RULE_RE = re.compile(r'^[a-z]+: [0-9].*$')
+FIELD_RE = re.compile(r'^[a-z ]+: [0-9].*$')
 ALL_RANGES = defaultdict(list)
 TICKETS = List[List[int]]
+YOUR_TICKET = []
 
 
-def collect_valid_tickets(notes: List[str]) -> TICKETS:
+def check_tickets_for_fields(notes: List[str]) -> Dict[int, set]:
     """Parse train ticket for validity and keep all valid tickets.
 
     Ignore your own ticket for this function.
@@ -20,82 +21,100 @@ def collect_valid_tickets(notes: List[str]) -> TICKETS:
             passengers' tickets
 
     Returns:
-        TICKETS: all valid tickets
+        Dict[int, set]: a dictionary of column indices to all matching fields
 
     """
-    valid_tickets = []
+    your_ticket = False
     nearby_tickets = False
+    fields = 0
     for line in notes:
-        if RULE_RE.match(line):
+        if FIELD_RE.match(line):
             field, ranges = line.split(': ')
             ranges = ranges.split(' or ')
             for r in ranges:
                 lower, upper = [int(n) for n in r.split('-')]
                 ALL_RANGES[field].append(range(lower, upper + 1))
+            fields += 1
+        elif line == 'your ticket:':
+            your_ticket = True
         elif line == 'nearby tickets:':
             nearby_tickets = True
+            col_fields = {i: set(ALL_RANGES.keys()) for i in range(fields)}
+        elif your_ticket:
+            ticket = [int(n) for n in line.split(',')]
+            YOUR_TICKET.extend(ticket)
+            your_ticket = False
         elif nearby_tickets:
-            numbers = [int(n) for n in line.split(',')]
-            valid_number_check = []
-            for number in numbers:
-                valid_number_check.append(
-                    any(
-                        [
-                            number in r
-                            for ranges in ALL_RANGES.values()
-                            for r in ranges
-                            ]
-                        )
-                    )
-            if all(valid_number_check):
-                valid_tickets.append(numbers)
+            ticket = [int(n) for n in line.split(',')]
+            ticket_cols = match_cols_to_fields(ticket)
+            # Silently discard invalid tickets.
+            if not ticket_cols:
+                continue
+            for col_idx, col in enumerate(ticket_cols):
+                col_fields[col_idx] = col_fields[col_idx] & col
 
-    return valid_tickets
+    return col_fields
 
 
-def match_col_to_rule(tickets: TICKETS) -> Dict[str, int]:
-    """Match columns in all valid tickets to their respective rules.
+def match_cols_to_fields(ticket: List[int]) -> Union[List[set], None]:
+    """Match columns in all valid tickets to their respective fields.
 
     Args:
-        tickets (TICKETS): a list of tickets
+        ticket (List[int]): a ticket containing numeric columns/fields
 
     Returns:
-        Dict[str, int]: a dictionary mapping rule name to column number
+        List[set]: a dictionary mapping rule name to column number
+        None: if ticket contains an invalid number
 
     """
-    rules = {}
-    ticket_cols_by_idx = {
-        i: [ticket[i] for ticket in tickets]
-        for i in range(len(tickets[0]))
-        }
+    cols = [set() for _ in ticket]
+    for i, number in enumerate(ticket):
+        for field, ranges in ALL_RANGES.items():
+            if any([number in r for r in ranges]):
+                cols[i].add(field)
+        if not cols[i]:
+            return
+    return cols
 
-    for idx, ticket_col in ticket_cols_by_idx.items():
-        for rule, ranges in ALL_RANGES.items():
-            if idx in rules.values():
-                continue
-            if all([any([col in r for r in ranges]) for col in ticket_col]):
-                rules[rule] = idx
-                break
 
-    return rules
+def determine_field_order(fields: Dict[int, set]) -> int:
+    """Determine field order from fields and print answer to 2020-12-16 b.
+
+    Args:
+        fields (Dict[int, set]): fields of sets of str
+
+    Returns:
+        int: the product of all six 'departure' related fields on your ticket
+
+    """
+    ordered_fields = {}
+    sorted_fields = sorted(fields.items(), key=lambda field: len(field[1]))
+    for col_idx, fields in sorted_fields:
+        for field in ordered_fields:
+            fields.discard(field)
+        field = fields.pop()
+        ordered_fields[field] = col_idx
+
+    product = 1
+    for field, idx in ordered_fields.items():
+        if field.startswith('departure'):
+            product *= YOUR_TICKET[idx]
+
+    return product
 
 
 def main() -> None:
     """Parse and read train ticket."""
     # Unlike other problems, 2020-12-16 b does not have a test answer.
-    test_answer = 0
-    file = config.TestFile(test_answer)
-    test = collect_valid_tickets(file.contents)
-    test = match_col_to_rule(test)
-    print(test)
-    # file.test(test)
+    # test_answer = 0
+    # file = config.TestFile(test_answer)
+    # test = check_tickets_for_fields(file.contents)
+    # # file.test(test)
 
     file = config.File()
-    result = collect_valid_tickets(file.contents)
-    test = match_col_to_rule(result)
-    print(test)
-
-    # config.LOGGER.info(result)
+    result = check_tickets_for_fields(file.contents)
+    result = determine_field_order(result)
+    config.LOGGER.info(result)
 
 
 if __name__ == '__main__':
